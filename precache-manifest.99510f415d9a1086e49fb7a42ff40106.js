81c11474f4fc2e50a587a46e093801178057c966
StackWolfed <sesilaso@gmail.com>
2019-01-16 17:26:08 +0100

    Updates

---
+++ precache-manifest.99510f415d9a1086e49fb7a42ff40106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6810cb0e44387835cfa9",
+    "url": "/AustinVODs/static/css/main.2c3aa995.chunk.css"
+  },
+  {
+    "revision": "6810cb0e44387835cfa9",
+    "url": "/AustinVODs/static/js/main.6810cb0e.chunk.js"
+  },
+  {
+    "revision": "5a845b3a2d6bbfa9d8fd",
+    "url": "/AustinVODs/static/css/1.f62af1ce.chunk.css"
+  },
+  {
+    "revision": "5a845b3a2d6bbfa9d8fd",
+    "url": "/AustinVODs/static/js/1.5a845b3a.chunk.js"
+  },
+  {
+    "revision": "af9be049d0e33d8766db",
+    "url": "/AustinVODs/static/js/runtime~main.af9be049.js"
+  },
+  {
+    "revision": "e89a561e64b20d0931f022c64c4729f6",
+    "url": "/AustinVODs/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/AustinVODs/precache-manifest.df2c00576f395cb864315523c8649591.js"
+  "/AustinVODs/precache-manifest.99510f415d9a1086e49fb7a42ff40106.js"
 );
 
 workbox.clientsClaim();
